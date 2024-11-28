0bdc056f4e1e13c7290261ea40b6c9138f5e6248
MisterTobias <moraisgoncalo498@gmail.com>
2024-11-28 01:30:32 +0000

    Update pokemon_constants.asm
    TO BE CONTINUED...

--- constants/pokemon_constants.asm
+++ constants/pokemon_constants.asm
@@ -37,8 +37,8 @@
 	const BLASTOISE          ; $1C
 	const PINSIR             ; $1D
 	const TANGELA            ; $1E
-	const_skip               ; $1F
-	const_skip               ; $20
+	const ANNIHILAPE         ; $1F TO BE ADDED MUST
+	const STEELIX            ; $20 TO BE ADDED
 	const GROWLITHE          ; $21
 	const ONIX               ; $22
 	const FEAROW             ; $23
@@ -56,26 +56,26 @@
 	const PSYDUCK            ; $2F
 	const DROWZEE            ; $30
 	const GOLEM              ; $31
-	const_skip               ; $32
+	const MAGNEZONE          ; $32 TO BE ADDED MUST
 	const MAGMAR             ; $33
-	const_skip               ; $34
+	const CROBAT             ; $34 TO BE ADDED MUST
 	const ELECTABUZZ         ; $35
 	const MAGNETON           ; $36
 	const KOFFING            ; $37
-	const_skip               ; $38
+	const KINGDRA            ; $38 TO BE ADDED MUST
 	const MANKEY             ; $39
 	const SEEL               ; $3A
 	const DIGLETT            ; $3B
 	const TAUROS             ; $3C
-	const_skip               ; $3D
-	const_skip               ; $3E
-	const_skip               ; $3F
+	const LICKILICKY         ; $3D TO BE ADDED
+	const RHYPERIOR          ; $3E TO BE ADDED MUST
+	const TANGROWTH          ; $3F TO BE ADDED MUST
 	const FARFETCHD          ; $40
 	const VENONAT            ; $41
 	const DRAGONITE          ; $42
-	const_skip               ; $43
-	const_skip               ; $44
-	const_skip               ; $45
+	const ELECTIVIRE         ; $43 TO BE ADDED
+	const MAGMOTAR           ; $44 TO BE ADDED
+	const SCIZOR             ; $45 TO BE ADDED
 	const DODUO              ; $46
 	const POLIWAG            ; $47
 	const JYNX               ; $48
@@ -85,23 +85,23 @@
 	const DITTO              ; $4C
 	const MEOWTH             ; $4D
 	const KRABBY             ; $4E
-	const_skip               ; $4F
-	const_skip               ; $50
-	const_skip               ; $51
+	const TOEDSCRUEL         ; $4F TO BE ADDED MUST
+	const PORYGON_Z          ; $50 TO BE ADDED
+	const A_RATICATE         ; $51 TO BE ADDED MUST SS ANNE
 	const VULPIX             ; $52
 	const NINETALES          ; $53
 	const PIKACHU            ; $54
 	const RAICHU             ; $55
-	const_skip               ; $56
-	const_skip               ; $57
+	const PRT_PIKACHU        ; $56 TO BE ADDED MUST
+	const PRT_EEVEE          ; $57 TO BE ADDED MUST
 	const DRATINI            ; $58
 	const DRAGONAIR          ; $59
 	const KABUTO             ; $5A
 	const KABUTOPS           ; $5B
 	const HORSEA             ; $5C
 	const SEADRA             ; $5D
-	const_skip               ; $5E
-	const_skip               ; $5F
+	const A_RAICHU           ; $5E TO BE ADDED MUST
+	const A_SANDSLASH        ; $5F TO BE ADDED MUST
 	const SANDSHREW          ; $60
 	const SANDSLASH          ; $61
 	const OMANYTE            ; $62
@@ -121,38 +121,38 @@
 	const WEEDLE             ; $70
 	const KAKUNA             ; $71
 	const BEEDRILL           ; $72
-	const_skip               ; $73
+	const A_NINETALES        ; $73 TO BE ADDED MUST
 	const DODRIO             ; $74
 	const PRIMEAPE           ; $75
 	const DUGTRIO            ; $76
 	const VENOMOTH           ; $77
-	const DEWGONG            ; $78
-	const_skip               ; $79
-	const_skip               ; $7A
+	const DEWGONG            ; $78 
+	const A_DUGTRIO          ; $79 TO BE ADDED MUST SS ANNE
+	const A_PERSIAN          ; $7A TO BE ADDED MUST SS ANNE
 	const CATERPIE           ; $7B
 	const METAPOD            ; $7C
 	const BUTTERFREE         ; $7D
 	const MACHAMP            ; $7E
-	const_skip               ; $7F
+	const H_ARCANINE         ; $7F TO BE ADDED MUST
 	const GOLDUCK            ; $80
 	const HYPNO              ; $81
 	const GOLBAT             ; $82
 	const MEWTWO             ; $83
 	const SNORLAX            ; $84
 	const MAGIKARP           ; $85
-	const_skip               ; $86
-	const_skip               ; $87
+	const A_GOLEM            ; $86 TO BE ADDED MUST SS ANNE
+	const G_RAPIDASH         ; $87 TO BE ADDED MUST
 	const MUK                ; $88
-	const_skip               ; $89
+	const G_SLOWBRO          ; $89 TO BE ADDED MUST
 	const KINGLER            ; $8A
 	const CLOYSTER           ; $8B
-	const_skip               ; $8C
+	const A_MUK              ; $8C TO BE ADDED MUST
 	const ELECTRODE          ; $8D
 	const CLEFABLE           ; $8E
 	const WEEZING            ; $8F
 	const PERSIAN            ; $90
 	const MAROWAK            ; $91
-	const_skip               ; $92
+	const H_ELECTRODE        ; $92 TO BE ADDED MUST
 	const HAUNTER            ; $93
 	const ABRA               ; $94
 	const ALAKAZAM           ; $95
@@ -162,13 +162,13 @@
 	const BULBASAUR          ; $99
 	const VENUSAUR           ; $9A
 	const TENTACRUEL         ; $9B
-	const_skip               ; $9C
+	const A_EXEGGUTOR        ; $9C TO BE ADDED MUST
 	const GOLDEEN            ; $9D
 	const SEAKING            ; $9E
-	const_skip               ; $9F
-	const_skip               ; $A0
-	const_skip               ; $A1
-	const_skip               ; $A2
+	const A_MAROWAK          ; $9F TO BE ADDED MUST
+	const G_WEEZING          ; $A0 TO BE ADDED MUST
+	const BLISSEY            ; $A1 TO BE ADDED
+	const P_TAUROS           ; $A2 TO BE ADDED MUST
 	const PONYTA             ; $A3
 	const RAPIDASH           ; $A4
 	const RATTATA            ; $A5
@@ -178,16 +178,16 @@
 	const GEODUDE            ; $A9
 	const PORYGON            ; $AA
 	const AERODACTYL         ; $AB
-	const_skip               ; $AC
+	const WUGTRIO            ; $AC TO BE ADDED MUST SS ANNE
 	const MAGNEMITE          ; $AD
-	const_skip               ; $AE
-	const_skip               ; $AF
+	const_skip               ; $AE FREE SLOT
+	const G_MR_MIME          ; $AF TO BE ADDED MUST
 	const CHARMANDER         ; $B0
 	const SQUIRTLE           ; $B1
 	const CHARMELEON         ; $B2
 	const WARTORTLE          ; $B3
 	const CHARIZARD          ; $B4
-	const_skip               ; $B5
+	const G_FARFETCHD        ; $B5 TO BE ADDED MUST
 	const FOSSIL_KABUTOPS    ; $B6
 	const FOSSIL_AERODACTYL  ; $B7
 	const MON_GHOST          ; $B8
